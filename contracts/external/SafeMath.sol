dd55e8964deaef3d044cc5ff45953d52fbc8e8d2
James Bond <rodrigoheitie@hotmail.com>
2022-03-06 15:51:57 +0100

    only for max()

--- contracts/external/SafeMath.sol
+++ contracts/external/SafeMath.sol
@@ -5,16 +5,4 @@ library SafeMath {
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return a >= b ? a : b;
     }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a * b;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a + b;
-    }
 }
