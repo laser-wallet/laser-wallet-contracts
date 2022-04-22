da5f8896a4782af4b9267852ef75da3386960685
Rodrigo Herrera Itie <80117772+rodrigoherrerai@users.noreply.github.com>
2022-04-22 10:51:22 -0500

    N3 "UNUSED DOMAIN SEPARATOR" (#14)
    merging pr

--- contracts/migration/Migration.sol
+++ contracts/migration/Migration.sol
@@ -8,8 +8,7 @@ import "../libraries/LaserWalletStorage.sol";
  *  @author Modified from Gnosis Safe.
  */
 contract Migration is LaserWalletStorage {
-    bytes32 private constant DOMAIN_SEPARATOR_TYPEHASH =
-        0x035aff83d86937d35b32e04f0ddc6ff469290eef2f1b692d8a815c89404d4749;
+
 
     address public immutable migrationSingleton;
     address public immutable safe120Singleton;
