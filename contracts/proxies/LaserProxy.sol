b5679e0d4fdb4c0d5865267d27a7e01e60655060
James Bond <rodrigoheitie@hotmail.com>
2022-06-06 23:03:43 +0200

    change of var name

--- contracts/proxies/LaserProxy.sol
+++ contracts/proxies/LaserProxy.sol
@@ -21,10 +21,17 @@ contract LaserProxy {
      * @dev Fallback function forwards all transactions and returns all received return data.
      */
     fallback() external payable {
-        address master = singleton;
+        address _singleton = singleton;
         assembly {
             calldatacopy(0, 0, calldatasize())
-            let success := delegatecall(gas(), master, 0, calldatasize(), 0, 0)
+            let success := delegatecall(
+                gas(),
+                _singleton,
+                0,
+                calldatasize(),
+                0,
+                0
+            )
             returndatacopy(0, 0, returndatasize())
             if eq(success, 0) {
                 revert(0, returndatasize())
