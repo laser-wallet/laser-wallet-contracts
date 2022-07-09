125bec81d7738b1e816da06eec7715e46c3ad539
James Bond <rodrigoheitie@hotmail.com>
2022-07-09 18:36:18 +0200

    more tests

--- test/storage/Laser.Storage.spec.ts
+++ test/storage/Laser.Storage.spec.ts
@@ -15,48 +15,52 @@ describe("Contract Storage", () => {
         await deployments.fixture();
     });
 
-    it("should have the singleton at storage slot 0", async () => {
-        const { address, wallet } = await walletSetup();
-        const slot0 = await getStorageSlot(address, "0");
-        const singleton = await wallet.singleton();
+    describe("On initialization", () => {
+        it("should have the singleton at storage slot 0", async () => {
+            const { address, wallet } = await walletSetup();
+            const slot0 = await getStorageSlot(address, "0");
+            const singleton = await wallet.singleton();
 
-        // last 20 bytes.
-        expect(singleton.toLowerCase()).to.equal(`0x${slot0.slice(26)}`);
+            // last 20 bytes.
+            expect(singleton.toLowerCase()).to.equal(`0x${slot0.slice(26)}`);
+        });
+
+        it("should have the owner at storage slot 1", async () => {
+            const { address, wallet } = await walletSetup();
+            const slot1 = await getStorageSlot(address, "1");
+            const owner = await wallet.owner();
+
+            // last 20 bytes.
+            expect(owner.toLowerCase()).to.equal(`0x${slot1.slice(26)}`);
+        });
+
+        it("should have recoveryOwnerCount at storage slot 2", async () => {
+            const { address, wallet } = await walletSetup();
+            const slot2 = await getStorageSlot(address, "2");
+
+            const recoveryOwners = await wallet.getRecoveryOwners();
+            const recoveryOwnerCount = recoveryOwners.length;
+
+            expect(recoveryOwnerCount.toString()).to.equal(slot2.slice(slot2.length - 1));
+        });
+
+        it("should have guardianCount at storage slot 3", async () => {
+            const { address, wallet } = await walletSetup();
+            const slot3 = await getStorageSlot(address, "3");
+
+            const guardians = await wallet.getGuardians();
+            const guardianCount = guardians.length;
+
+            expect(guardianCount.toString()).to.equal(slot3.slice(slot3.length - 1));
+        });
+
+        it("should have timeLock at storage slot 4", async () => {
+            const { address, wallet } = await walletSetup();
+            const slot4 = await getStorageSlot(address, "4");
+
+            expect(slot4.toString()).to.equal("0x0000000000000000000000000000000000000000000000000000000000000000");
+        });
     });
 
-    it("should have the owner at storage slot 1", async () => {
-        const { address, wallet } = await walletSetup();
-        const slot1 = await getStorageSlot(address, "1");
-        const owner = await wallet.owner();
-
-        // last 20 bytes.
-        expect(owner.toLowerCase()).to.equal(`0x${slot1.slice(26)}`);
-    });
-
-    it("should have recoveryOwnerCount at storage slot 2", async () => {
-        const { address, wallet } = await walletSetup();
-        const slot2 = await getStorageSlot(address, "2");
-
-        const recoveryOwners = await wallet.getRecoveryOwners();
-        const recoveryOwnerCount = recoveryOwners.length;
-
-        expect(recoveryOwnerCount.toString()).to.equal(slot2.slice(slot2.length - 1));
-    });
-
-    it("should have guardianCount at storage slot 3", async () => {
-        const { address, wallet } = await walletSetup();
-        const slot3 = await getStorageSlot(address, "3");
-
-        const guardians = await wallet.getGuardians();
-        const guardianCount = guardians.length;
-
-        expect(guardianCount.toString()).to.equal(slot3.slice(slot3.length - 1));
-    });
-
-    it("should have timeLock at storage slot 4", async () => {
-        const { address, wallet } = await walletSetup();
-        const slot4 = await getStorageSlot(address, "4");
-
-        expect(slot4.toString()).to.equal("0x0000000000000000000000000000000000000000000000000000000000000000");
-    });
+    describe("On change", () => {});
 });
