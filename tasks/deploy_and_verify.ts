057898285dc4174e850760961e1d56e889982af8
James Bond <rodrigoheitie@hotmail.com>
2022-08-17 15:59:32 -0500

    cleaning it

--- tasks/deploy_and_verify.ts
+++ tasks/deploy_and_verify.ts
@@ -1,38 +1,10 @@
 import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
 import { task } from "hardhat/config";
 
-task("deploy-and-verify", "Deploys all Laser contracts and verifies them on Etherscan").setAction(async (_, hre) => {
+task("deploy-and-verify", "Deploys all Laser contracts and verifies them on Etherscan.").setAction(async (_, hre) => {
     await hre.run("deploy");
-
-    const { LaserWallet, LaserFactory, LaserVault, LaserHelper, LaserRegistry, LaserModuleSSR, LaserMasterGuard } =
-        await hre.deployments.all();
-
-    await hre.run("verify:verify", {
-        address: LaserWallet.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserFactory.address,
-        constructorArguments: [LaserWallet.address],
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserHelper.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserVault.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserRegistry.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserModuleSSR.address,
-    });
-
-    await hre.run("verify:verify", {
-        address: LaserMasterGuard.address,
-    });
+    await hre.run("etherscan-verify", { forceLicense: true, license: "LGPL-3.0" });
 });
+
+export {};
